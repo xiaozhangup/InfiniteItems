22c394e5c2d68e4b24eeb157246fac94eb9f788e
xiaozhangup <xiaozhangshiw@163.com>
2021-11-30 23:22:30 +0800

    update

---
+++ src/main/java/me/xiaozhangup/infiniteitems/EventControl.java
@@ -0,0 +1,14 @@
+package me.xiaozhangup.infiniteitems;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class EventControl implements Listener {
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e) {
+        //Code
+    }
+
+}
--- src/main/java/me/xiaozhangup/infiniteitems/Main.java
+++ src/main/java/me/xiaozhangup/infiniteitems/Main.java
@@ -1,12 +1,14 @@
 package me.xiaozhangup.infiniteitems;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        super.onEnable();
+        getLogger().info(this + " LOADED...");
+        Bukkit.getPluginManager().registerEvents(new EventControl(), this);
     }
 
 }
