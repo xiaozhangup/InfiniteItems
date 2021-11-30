900ec702428449114ac66f8141fbd6edc901b651
xiaozhangup <xiaozhangshiw@163.com>
2021-11-30 21:28:02 +0800

    update

--- src/main/java/me/xiaozhangup/infiniteitems/Main.java
+++ src/main/java/me/xiaozhangup/infiniteitems/Main.java
@@ -3,4 +3,10 @@ package me.xiaozhangup.infiniteitems;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
 }
