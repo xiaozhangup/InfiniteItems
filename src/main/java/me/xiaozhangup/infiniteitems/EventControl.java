962431029ae0fd79020958b4ab95843b52765d27
xiaozhangup <xiaozhangshiw@163.com>
2021-12-06 21:04:14 +0800

    update

--- src/main/java/me/xiaozhangup/infiniteitems/EventControl.java
+++ src/main/java/me/xiaozhangup/infiniteitems/EventControl.java
@@ -1,12 +1,9 @@
 package me.xiaozhangup.infiniteitems;
 
-import org.bukkit.Location;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.event.player.PlayerItemDamageEvent;
